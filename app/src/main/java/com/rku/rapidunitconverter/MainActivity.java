1c92f7fde1f7162c2f52565a61cfa3802fd598fa
kishan <ksakhiya315@rku.ac.in>
2022-05-28 18:28:45 +0530

    Running Success

--- app/src/main/java/com/rku/rapidunitconverter/MainActivity.java
+++ app/src/main/java/com/rku/rapidunitconverter/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     CardView cvspeed, cvlength, cvmass, cvtime, cvdigitalstorage, cvarea;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
